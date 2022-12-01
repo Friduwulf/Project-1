a605c8050f1563b39ea57ebcc359df071388c26b
Devin Kearney <devinkearney@gmail.com>
2022-12-01 15:20:45 -0400

    Fetch data from wiki API and show it on page complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,71 +2,100 @@
 ///Wiki API Integration///
 //////////////////////////
 
-var endpoint = 'https://https://en.wikipedia.org/w/api.php';
+var endpoint = 'https://en.wikipedia.org/w/api.php';
 
 var parameters = {
-    origin:'*',
+    origin: '*',
     format: 'json',
     action: 'query',
     prop: 'extracts',
-    exintor: true,
-    exsectionformat: 'plain',
+    exchars: 250,
+    exintro: true,
+    explaintext: true,
     generator: 'search',
     gsrlimit: 1,
-}
+};
 
 var submitButton = document.querySelector('#search');
 var input = document.querySelector('#searchText');
 var errorSpan = document.querySelector('#error');
-var resultsContainer = document.querySelector('#results');
+var wikiTextBox = document.querySelector('#wikiText');
+var wikiTitleBox = document.querySelector('.wikiTitle');
 
-var disableUi = () => {
+const disableUi = () => {
     input.disabled = true;
     submitButton.disabled = true;
-}
+};
 
-var enableUi = () => {
+const enableUi = () => {
     input.disabled = false;
     submitButton.disabled = false;
-}
+};
 
-var clearPreviousResults = () => {
-    resultsContainer.innerHTML = '';
+const clearPreviousResults = () => {
+    wikiTextBox.innerHTML = '';
     errorSpan.innerHTML = '';
-}
+};
 
-var isInputEmpty = () => {
+const isInputEmpty = input => {
     if (!input || input === '') return true;
     return false;
+};
+
+const showError = (error) => {
+    alert(error);
+};
+
+const enterKeyPress = (e) => {
+    if (e.key === 'Enter') {
+        searchArticle();
+    }
+};
+
+const wikiResult = results => {
+    console.log(results);
+    wikiTextBox.textContent = results[0].extract;
+    wikiTitleBox.textContent = results[0].articleTitle;
 }
 
-var showError = (error) => {
-    errorSpan.innerHTML = `${error}`;
-}
 
+const getExtract = pages => {
+    var results = Object.values(pages).map(page => ({
+        extract: page.extract,
+        articleTitle: page.title,
+    }));
 
+    wikiResult(results);
+};
+
+const searchArticle = async () => {
+    var searchValue = input.value;
+    if (isInputEmpty(searchValue)) return;
+    parameters.gsrsearch = searchValue;
+    disableUi();
+    try {
+        const { data } = await axios.get(endpoint,{ params:parameters });
+        if (data.error) throw new Error(data.error.info);
+        getExtract(data.query.pages);
+        enableUi();
+    } catch (error) {
+        showError(error);
+        enableUi();
+    }
+};
+
+const searchEventHandler = () => {
+    input.addEventListener('keydown', enterKeyPress);
+    submitButton.addEventListener('click', searchArticle);
+};
+
+searchEventHandler();
 
 ////////////////////////
 ///HTML functionality///
 ////////////////////////
 
-var enterKeyPress = (e) => {
-    if (e.key === 'Enter') {
-        searchArticle();
-    }
-}
 
-var searchArticle = () => {
-    var searchValue = input.value;
-    console.log(searchValue);
-}
-
-var searchEventHandler = () => {
-    input.addEventListener('keydown', enterKeyPress);
-    submitButton.addEventListener('click', searchArticle);
-}
-
-searchEventHandler();
 
 var instance = M.Carousel.init({
   fullWidth: true,
