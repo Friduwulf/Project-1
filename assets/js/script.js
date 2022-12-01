625ecbb6a657781ae7a3a1b5ac8cb2808188ba74
Gabe Morris <gabemorris1042@gmail.com>
2022-12-01 18:49:43 -0400

    removed extra jquery link

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,11 +113,10 @@ var instance = M.Carousel.init({
 
 // Gabes youtube API key: AIzaSyBGcs-zAc9WhKvOuKcSsyF8KXUopPe7d6U
 
-// An example fetch for dogs using the youtube data api
-// fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=dogs&key=AIzaSyBGcs-zAc9WhKvOuKcSsyF8KXUopPe7d6U`)
-// .then(function(response) {
-//   return response.json()
-// })
-// .then(function(data) {
-//   console.log(data)
-// })
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${"dogs"}&key=AIzaSyBGcs-zAc9WhKvOuKcSsyF8KXUopPe7d6U`)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data)
+})
