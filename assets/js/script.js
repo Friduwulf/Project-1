7bc08d318ae6210ef7f038c96a4801eef35f6972
Devin Kearney <devinkearney@gmail.com>
2022-12-08 17:18:24 -0400

    Commented all of my lines of code, as well as made some symantic edits.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,42 @@
 //////////////////////////
 ///Wiki API Integration///
 //////////////////////////
+//Initiates an array for local storage use.
+var storedSearches = [];
+//HTML targets for JS use.
+var submitButton = document.querySelector('#search');
+var input = document.querySelector('#searchText');
+var wikiTextBox = document.querySelector('#wikiText');
+var wikiTitleBox = document.querySelector('.wikiTitle');
+var wikiLink = document.querySelector('.wikiLink');
+var previousSearchButton = document.querySelector('#last_search');
+//Allows the search to be disabled.
+const disableUi = () => {
+    input.disabled = true;
+    submitButton.disabled = true;
+};
+//Enables the search function.
+const enableUi = () => {
+    input.disabled = false;
+    submitButton.disabled = false;
+};
+//Prevents searches of empty strings.
+const isInputEmpty = input => {
+    if (!input || input === '') return true;
+    return false;
+};
+//Runs the search wiki and youtube functions on enter press.
+const enterKeyPress = (e) => {
+    if (e.key === 'Enter') {
+        searchArticle();
+        searchVideo();
+    }
+};
+//Initiates a variable for the value of the search.
 var searchValue = null;
+//The variable that is the endpoint of the fetch request.
 var endpoint = 'https://en.wikipedia.org/w/api.php';
-
+//An object of parameters for use in axios to create the required wikipedia API fetch request.
 var parameters = {
     origin: '*',
     format: 'json',
@@ -14,55 +47,25 @@ var parameters = {
     generator: 'search',
     gsrlimit: 1,
 };
-
-var storedSearches = [];
-
-var submitButton = document.querySelector('#search');
-var input = document.querySelector('#searchText');
-var wikiTextBox = document.querySelector('#wikiText');
-var wikiTitleBox = document.querySelector('.wikiTitle');
-var wikiLink = document.querySelector('.wikiLink');
-var previousSearchButton = document.querySelector('#last_search');
-
-const disableUi = () => {
-    input.disabled = true;
-    submitButton.disabled = true;
-};
-
-const enableUi = () => {
-    input.disabled = false;
-    submitButton.disabled = false;
-};
-
-const isInputEmpty = input => {
-    if (!input || input === '') return true;
-    return false;
-};
-
-const enterKeyPress = (e) => {
-    if (e.key === 'Enter') {
-        searchArticle();
-        searchVideo();
-    }
-};
-
+//Sets the article title, textbox and wiki link to the proper values returned by the fetch request.
 const wikiResult = results => {
     console.log(results);
     wikiTextBox.textContent = results[0].extract;
     wikiTitleBox.textContent = results[0].articleTitle;
     wikiLink.setAttribute("href", `https://en.wikipedia.org/?curid=${results[0].link}`);
 };
-
+//Gets the correct parts of the fetched wiki data.
 const getExtract = pages => {
     var results = Object.values(pages).map(page => ({
         extract: page.extract,
         articleTitle: page.title,
         link: page.pageid,
     }));
-
     wikiResult(results);
 };
-
+//Builds the fetch request using axios, disables search function until the fetch operation finishes, 
+//pushes the searched string to the storedSearches array, stores the array in local storage, returns the fetched data, 
+//and runs an error modal if the return is invalid, then re-enables the search functionality.
 const searchArticle = async () => {
     searchValue = input.value;
     if (isInputEmpty(searchValue)) return;
@@ -80,33 +83,26 @@ const searchArticle = async () => {
         enableUi();
     }
 };
-
 // displayVideo is used to change the video element in the html
 let displayVideo;
 // saveCurrentData is used to hand over the fetched data over to changeVideo function, instead of fetching multiple times
 let saveCurrentData;
 const searchVideo = () => {
     currentVideo = 0;
-
     if (isInputEmpty(searchValue)) return;
     searchValue = input.value;
-
     disableUi();
-
     // Fetches video data based on searchValue
     fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=5&q=${searchValue}&key=AIzaSyApu7PF3orxR1Krl_fgkehmLRmr5jhWPp0`)
     .then(function(response) {
     return response.json();
     })
     .then(function(data) {
-
     // Pass the data from the fetch to saveCurrentData for later use
     saveCurrentData = data;  
-
     try {
         // Change to video element in the html to what the user just searched for
         displayVideo = $("#player").attr("src", `https://www.youtube.com/embed/${data.items[0].id.videoId}`);
-
         console.log(data)
         enableUi();
     } catch (error) {
@@ -117,7 +113,6 @@ const searchVideo = () => {
     // Clears the input value after submitting a search so the user doesn't have to delete anything to search something else
     input.value = '';
 }
-
 let currentVideo = 0;
 const changeVideo = (event) => {
 
@@ -153,7 +148,7 @@ const changeVideo = (event) => {
         }
     }
 }
-
+//Gets the previously searched string from local storage and puts it in the search bar, prevents this action if there are no previous searches.
 const searchLast = () => {
     let prevSearch = JSON.parse(localStorage.getItem('previousSearch'));
     console.log(prevSearch);
@@ -161,21 +156,23 @@ const searchLast = () => {
         input.value = prevSearch[prevSearch.length - 2];
     }
 };
-
+//Changes the displayed video on button press.
 let nextVideo = $("#nextVideo").on("click", changeVideo);
 let prevVideo = $("#prevVideo").on("click", changeVideo);
-
+//Event handler for all button interaction and pressing enter for search.
 const searchEventHandler = () => {
     input.addEventListener('keydown', enterKeyPress);
     submitButton.addEventListener('click', searchArticle);
     submitButton.addEventListener('click', searchVideo);
     previousSearchButton.addEventListener('click', searchLast);
 };
-
+//Initiates modals on page load.
 $(document).ready(function() {
     $('.modal').modal();
 });
-
+//Initiates event handler.
 searchEventHandler();
 // Devin's youtube API key: AIzaSyApu7PF3orxR1Krl_fgkehmLRmr5jhWPp0
 // Gabes youtube API key: AIzaSyBGcs-zAc9WhKvOuKcSsyF8KXUopPe7d6U
+//We use multiple API keys as the YouTube API has a limit to the number of requests per day when using the free version. 
+//If the videos are failing to load, exchange the API key on line 96.
